a59eb2064f722d12b94dc03b10cd636e3dc629b2
Tobias Wilker <twilker@phoenixcontact.com>
2022-09-01 06:10:34 +0200

    First interation functions.

--- functions/index.js
+++ functions/index.js
@@ -1,143 +1,212 @@
-const fetch = require('node-fetch');
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
+/* eslint-disable require-jsdoc */
+const fetch = require("node-fetch");
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Realtime Database.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+/* const serviceAccount = require("./service_acounts/serviceAccount.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kawaii-passion-hub-orders-default-rtdb.firebaseio.com",
+}); */
 admin.initializeApp();
 
-exports.mirrorCron = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
-    console.log('This will be run every 5 minutes!');
+const authApp = admin.initializeApp({projectId: "kawaii-passion-hub-auth"},
+    "kawaii-passion-hub-auth");
 
-    return null;
+/* exports.mirrorCron = functions.pubsub.schedule('every 5 minutes')
+.onRun((context) => {
+  console.log('This will be run every 5 minutes!');
+
+  return null;
+}); */
+
+exports.authenticate = functions.https.onCall(async (data, _) => {
+  const originalJwt = data.jwt;
+  let token = {};
+  try {
+    token = await admin.auth(authApp).verifyIdToken(originalJwt);
+  } catch (error) {
+    console.error(error);
+    throw new functions.https.HttpsError("invalid-argument",
+        "The provided JWT was invalid", error);
+  }
+  const uid = token.user_id;
+  const customClaims = {};
+  if (token.whitelisted !== undefined) {
+    customClaims.whitelisted = token.whitelisted;
+  }
+  try {
+    const customToken = await admin.auth().createCustomToken(uid, customClaims);
+    return customToken;
+  } catch (error) {
+    console.error(error);
+    throw new functions.https.HttpsError("internal",
+        "Could not create custom token.", error);
+  }
 });
 
-// On sign up.
-exports.processSignUp = functions.auth.user().onCreate(async (user) => {
-    var db = admin.database();
-    var whitelisted = await db.ref('users/whitelisted').get();
+exports.mirror = functions
+    .runWith({
+      secrets: ["API_ID", "API_SECRET"],
+      timeoutSeconds: 540,
+    })
+    .https.onRequest(async (_data, res) => {
+      const orders = await fetchOrders();
 
-    // Check if user meets role criteria.
-    if (user.email &&
-        user.emailVerified &&
-        whitelisted.val().includes(user.email)
-    ) {
-      const customClaims = {
-        whitelisted: true
-      };
-  
-      try {
-        // Set custom user claims on this newly created user.
-        await admin.auth().setCustomUserClaims(user.uid, customClaims);
-  
-        // Update real-time database to notify client to force refresh.
-        const metadataRef = db.ref('metadata/' + user.uid);
-  
-        // Set the refresh time to the current UTC timestamp.
-        // This will be captured on the client to force a token refresh.
-        await  metadataRef.set({refreshTime: new Date().getTime()});
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  });
+      const db = admin.database();
+      await db.ref("orders").update(orders,
+          (error) => {
+            if (error) {
+              throw new functions.https.HttpsError("internal",
+                  "Could not write to database", error);
+            }
+          });
 
-exports.mirrorTest = functions.runWith({ secrets: ["API_ID", "API_SECRET"] }).https.onRequest(async (req, res) => {
-    let body = '';
-
-    await shopLogin().then(data => body = data).catch(err => res.status(400).end(JSON.stringify(err)));
-
-    if (!body || !body.token_type || !body.access_token) {
-        return res.status(404).end('Unable to fetch the app data :/');
-    }
-
-    const auth = `${body.token_type} ${body.access_token}`;
-    
-    await getProducts(auth).then(data => body = data).catch(err => res.status(400).end(JSON.stringify(err)));
-    var products = Object.fromEntries(body.data.map((p) => [p.productNumber.replace(/\./g,"-"), p]));
-
-    if (!body || !body.data) {
-        return res.status(404).end('Unable to fetch the app data :/');
-    }
-    
-    body = '';
-    await getOrders(auth).then(data => body = data).catch(err => res.status(400).end(JSON.stringify(err)));
-    var orders = Object.fromEntries(body.data.map((p) => [p.orderNumber.replace(/\./g,"-"), p]));
-
-    if (!body || !body.data) {
-        return res.status(404).end('Unable to fetch the app data :/');
-    }
-
-    var db = admin.database();
-    await db.ref().update({
-        products: products,
-        orders: orders
-    }, (error) => {
-        if (error) {
-          body = error;
-        }
-        else {
-            body = "Sucessfully added data";
-        }
+      res.send("Successfull");
     });
 
-    res.send(body);
-});
+exports.resync = functions
+    .runWith({
+      secrets: ["API_ID", "API_SECRET"],
+      timeoutSeconds: 540,
+    })
+    .https.onRequest(async (_data, res) => {
+      const orders = await fetchOrders();
 
-exports.logOrderCreation = functions.database.ref('orders/{id}')
-    .onCreate((snapshot, context) => {
-        console.log(`Order created occured ${snapshot.toJSON()}`);
-        return null;
+      const db = admin.database();
+      await db.ref("orders").set(orders,
+          (error) => {
+            if (error) {
+              throw new functions.https.HttpsError("internal",
+                  "Could not write to database", error);
+            }
+          });
+
+      res.send("Successfull");
     });
 
-exports.logorderStateChange = functions.database.ref('orders/{id}/stateId')
-    .onUpdate((change, context) => {
-        console.log(`Order state changed ${change.before.toJSON()} -> ${change.after.toJSON()}`);
-        return null;
-    });
+async function fetchOrders() {
+  const auth = await shopLogin();
+  const body = await getOrders(auth);
 
-async function getProducts(auth) {
-    return await getShopApiResponse(auth, '/api/product?');
+  const orders = Object.fromEntries(body.data
+      .map((p) => [p.attributes.orderNumber.replace(/\./g, "-"),
+        p.attributes]));
+  const included = Object.fromEntries(body.included
+      .map((p) => [p.id, p.attributes]));
+
+  console.log(`Completing ${Object.keys(orders).length} orders.`);
+  let remaining = Object.keys(orders).length;
+  await parallelForEach(body.data, async (order) => {
+    remaining -= 1;
+    const orderNumber = order.attributes.orderNumber.replace(/\./g, "-");
+    console.log(`Processing ${orderNumber}. ${remaining} unprocessed.`);
+    orders[orderNumber].stateMachineState = included[order.attributes.stateId];
+    orders[orderNumber].orderCustomer = included[order.relationships
+        .orderCustomer.data.id];
+    orders[orderNumber].lineItemsUrl = order.relationships
+        .lineItems.links.related;
+    orders[orderNumber].addressUrl = order.relationships
+        .addresses.links.related;
+    await completeOrder(orders[orderNumber]);
+  }, 5);
+
+  return orders;
+}
+
+async function completeOrder(order) {
+  const auth = await shopLogin();
+
+  let details = await getShopApiResponse(auth, order.lineItemsUrl, true);
+  const items = Object.fromEntries(details.data
+      .map((p) => [p.id, p.attributes]));
+  order.lineItems = items;
+
+  details = await getShopApiResponse(auth, order.addressUrl, true);
+  order.address = details.data[0].attributes;
+
+  details = await getShopApiResponse(auth, details.data[0].relationships
+      .country.links.related, true);
+  order.address.country = details.data[0].attributes;
+}
+
+async function parallelForEach(array, callback, maxParallel) {
+  const chunks = partition(array, maxParallel);
+  await Promise.all(chunks.map((c) => asyncForEach(c, callback)));
+}
+
+function partition(arr, partitions) {
+  const chunkSize = arr.length / partitions;
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
 
 async function getOrders(auth) {
-    return await getShopApiResponse(auth, '/api/order?');
+  return await getShopApiResponse(auth, "/api/order?");
 }
 
-async function getShopApiResponse(auth, endpoint) {
-    return await fetch(`${process.env.SHOP_URL}${endpoint}`,
-    {
-        method: 'GET',
+async function getShopApiResponse(auth, endpoint, absolute=false) {
+  const url = absolute ? endpoint : `${process.env.SHOP_URL}${endpoint}`;
+  const body = await fetch(url,
+      {
+        method: "GET",
         headers: {
-            'Authorization': auth,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    }).then (response => {
-        if (response.status != 200){
-            throw `fetch Error: ${response.status} ${response.statusText}`
-        }
-        return response.json();
-    });
+          "Authorization": auth,
+          "Content-Type": "application/json",
+          "Accept": "*/*",
+        },
+      }).then((response) => {
+    if (response.status != 200) {
+      throw new functions.https.HttpsError("permission-denied",
+          `Shop API Error: ${url} ${response.status} ${response.statusText}`);
+    }
+    return response.json();
+  });
+
+  if (!body || !body.data) {
+    throw new functions.https.HttpsError("internal",
+        `Unexpected ${url} data :/`, body);
+  }
+
+  return body;
 }
 
 async function shopLogin() {
-    return await fetch(`${process.env.SHOP_URL}/api/oauth/token`,
-    {
-        method: 'POST',
+  const body = await fetch(`${process.env.SHOP_URL}/api/oauth/token`,
+      {
+        method: "POST",
         headers: {
-            'Authorization': '',
-            'Content-Type': 'application/json'
+          "Authorization": "",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            grant_type: "client_credentials",
-            client_id: process.env.API_ID,
-            client_secret: process.env.API_SECRET
-        })
-    }).then (response => {
-        if (response.status != 200){
-            throw `fetch Error: ${response.status} ${response.statusText}`
-        }
-        return response.json();
-    });
+          grant_type: "client_credentials",
+          client_id: process.env.API_ID,
+          client_secret: process.env.API_SECRET,
+        }),
+      }).then((response) => {
+    if (response.status != 200) {
+      throw new functions.https.HttpsError("permission-denied",
+          `Authentification Error: ${response.status} ${response.statusText}`);
+    }
+    return response.json();
+  });
+
+  if (!body || !body.token_type || !body.access_token) {
+    throw new functions.https.HttpsError("internal",
+        "Unexpected auth data :/", body);
+  }
+
+  return `${body.token_type} ${body.access_token}`;
 }
